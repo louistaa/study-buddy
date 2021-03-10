b85ecc44a3cdc0014848eca1e2ccda3adf0e4165
Saatvik Arya <aryasaatvik@gmail.com>
2021-03-09 17:41:25 -0800

    deploy client

--- clients/deploy.sh
+++ clients/deploy.sh
@@ -1,3 +1,3 @@
 ./build.sh
 docker push aryasaatvik/sb-client
-ssh -i "~/sb_client.pem" saatvik@ec2-3-90-214-253.compute-1.amazonaws.com < aws.sh
+ssh -i "~/sb_client.pem" saatvik@studybuddy.kaylalee.me < aws.sh
