9130a8b59ebee2a56da69b3a5e0c97b6df46aaaa
Saatvik Arya <aryasaatvik@gmail.com>
2021-03-12 01:31:41 -0800

    fix cors

--- servers/gateway/handlers/cors.go
+++ servers/gateway/handlers/cors.go
@@ -24,5 +24,9 @@ func (c *CORS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 	w.Header().Set("Access-Control-Max-Age", "600")
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 	c.Handler.ServeHTTP(w, r)
 }
