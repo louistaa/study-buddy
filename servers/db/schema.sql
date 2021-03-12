7339ac255dcab5a9ac8a1e529488167f0b99cba2
Saatvik Arya <aryasaatvik@gmail.com>
2021-03-12 08:07:36 -0800

    fix

--- servers/db/schema.sql
+++ servers/db/schema.sql
@@ -30,7 +30,7 @@ create table if not exists StudentCourse (
     CourseID int,
     FOREIGN KEY (StudentID) REFERENCES Students(ID),
     FOREIGN KEY (CourseID) REFERENCES Courses(ID),
-    PRIMARY KEY (CourseID, ExpertID)
+    PRIMARY KEY (CourseID, StudentID)
 );
 
 create table if not exists CourseExpert (
