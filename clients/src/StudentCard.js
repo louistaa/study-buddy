6a6ec90364a62cf2b53e93940e3a035ba42a4dc8
Louis Ta <louistaa@gmail.com>
2021-03-13 00:54:11 -0800

    fix some css

--- clients/src/StudentCard.js
+++ clients/src/StudentCard.js
@@ -10,20 +10,15 @@ export default function StudentCard(props) {
     setRedirectTo(props.person)
   }
 
-  // if (redirectTo) {
-  //   return <Redirect push to={'/chats/' + redirectTo} />
-  // }
-
   return (
     <div className="col-9">
       <div className="card" onClick={handleClick}>
         <div className="card-body">
           <h5 className="card-title">{props.person}</h5>
           <h6 className="card-subtitle mb-2 text-muted">Username: {props.username} Major: {props.major}</h6>
-          <p className="card-text">Phone number: {props.phonenumber} Email: {props.email}</p>
-        </div>
-        <div className="contact">
-          <button type="button" class="btn btn-outline-primary">View Profile</button>
+          <p className="card-text">Phone number: {props.phonenumber}</p> 
+          <p> Email: {props.email}</p>
+          Click to view profile
         </div>
       </div>
     </div>
